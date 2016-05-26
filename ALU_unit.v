978fc080052778fb1c272574d7d1a99579cb277b
heszgergely <heszgergely@gmail.com>
2016-05-26 21:01:46 +0200

    CPU.v hozuzáadva

--- ALU_unit.v
+++ ALU_unit.v
@@ -18,10 +18,7 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module ALU_unit(
-      input         clk,
-      input         rst,
-		
+module ALU_unit(		
 		input  [7:0]  opA,
 		input  [7:0]  opB,
 		output [7:0]  outQ,
@@ -44,32 +41,26 @@ parameter NAND   = 4'h7;
 parameter OR     = 4'h8;
 parameter NOR    = 4'h9;
 
-reg outReg;
-
 always  @ ( opA, opB, cin, opcode)  
 case (opcode)
-	ADD: 	{cout,outReg} <= opA + opB + cin;		
-	SUB:	outReg <= opA + opB + carryIn;
-	LSHIFT: outReg <= {opA[6:0],cin} ;
-	RSHIFT: outReg <= { cin, opB[7:1] };
-	XOR:  outReg <= opA^opB;
-	AND:  outReg <= opA&opB;
-	NAND: outReg <= ~(opA&opB);
-	OR:	outReg <= opA|opB;
-	NOR:  outReg <= ~(opA|opB);
+	ADD: 	{cout,outQ} <= opA + opB + cin;		
+	SUB:	outQ <= opA - opB + cin;
+	LSHIFT: outQ <= {opA[6:0],cin} ;
+	RSHIFT: outQ <= { cin, opB[7:1] };
+	XOR:  outQ <= opA^opB;
+	AND:  outQ <= opA&opB;
+	NAND: outQ <= ~(opA&opB);
+	OR:	outQ <= opA|opB;
+	NOR:  outQ <= ~(opA|opB);
 	CMP: 
 	begin
-		if(opA == opB) outReg<=8'h01;
-		else if (opA>opB) outReg<=8'h02;
-		else if (opA<opB) outReg=8'h03;	
+		if(opA == opB) outQ<=8'h01;
+		else if (opA>opB) outQ<=8'h02;
+		else if (opA<opB) outQ=8'h03;	
 	end
-	default:  outReg <= 8'h0;
+	default:  outQ <= 8'h0;
 	
 endcase 
-
-always @ (posedge clk)
-	outQ <= outReg;
-
 
 
 endmodule
---
+++ CPU.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:36:05 05/26/2016 
+// Design Name: 
+// Module Name:    CPU 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module CPU(
+	input [7:0]  	data_in,
+	input [7:0]  	opcode,
+	input  			cin,
+	input 			cout,
+	input 			load,
+	
+	output [7:0]  	data_out,
+    );
+
+
+reg [7:0] operands
+
+endmodule
